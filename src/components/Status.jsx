f5ecb05c44c7d9abb11fbe91b61021c46086bb23
Ahindra D <official.ahindra@gmail.com>
2022-07-01 15:11:30 +0530

    linting

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,27 +1,27 @@
 function Status(board) {
     // write your code here
     let result = "";
-    if (((board[0][0] == "X") && (board[0][1] == "X") && (board[0][2] == "X")) ||
-        ((board[1][0] == "X") && (board[1][1] == "X") && (board[1][2] == "X")) || // rows matching
-        ((board[2][0] == "X") && (board[2][1] == "X") && (board[2][2] == "X")) ||
+    if (((board[0][0] === "X") && (board[0][1] === "X") && (board[0][2] === "X")) ||
+        ((board[1][0] === "X") && (board[1][1] === "X") && (board[1][2] === "X")) || // rows matching
+        ((board[2][0] === "X") && (board[2][1] === "X") && (board[2][2] === "X")) ||
 
-        ((board[0][0] == "X") && (board[1][0] == "X") && (board[2][0] == "X")) ||
-        ((board[0][1] == "X") && (board[1][1] == "X") && (board[2][1] == "X")) || // columns matching
-        ((board[0][2] == "X") && (board[1][2] == "X") && (board[2][3] == "X")) ||
+        ((board[0][0] === "X") && (board[1][0] === "X") && (board[2][0] === "X")) ||
+        ((board[0][1] === "X") && (board[1][1] === "X") && (board[2][1] === "X")) || // columns matching
+        ((board[0][2] === "X") && (board[1][2] === "X") && (board[2][3] === "X")) ||
 
-        ((board[0][0] == "X") && (board[1][1] == "X") && (board[2][2] == "X")) || // diagonals
-        ((board[0][2] == "X") && (board[1][1] == "X") && (board[2][0] == "X"))) {
+        ((board[0][0] === "X") && (board[1][1] === "X") && (board[2][2] === "X")) || // diagonals
+        ((board[0][2] === "X") && (board[1][1] === "X") && (board[2][0] === "X"))) {
         result = "X wins";
-    } else if (((board[0][0] == "O") && (board[0][1] == "O") && (board[0][2] == "O")) ||
-        ((board[1][0] == "O") && (board[1][1] == "O") && (board[1][2] == "O")) || // rows matching
-        ((board[2][0] == "O") && (board[2][1] == "O") && (board[2][2] == "O")) ||
+    } else if (((board[0][0] === "O") && (board[0][1] === "O") && (board[0][2] === "O")) ||
+        ((board[1][0] === "O") && (board[1][1] === "O") && (board[1][2] === "O")) || // rows matching
+        ((board[2][0] === "O") && (board[2][1] === "O") && (board[2][2] === "O")) ||
 
-        ((board[0][0] == "O") && (board[1][0] == "O") && (board[2][0] == "O")) ||
-        ((board[0][1] == "O") && (board[1][1] == "O") && (board[2][1] == "O")) || // columns matching
-        ((board[0][2] == "O") && (board[1][2] == "O") && (board[2][3] == "O")) ||
+        ((board[0][0] === "O") && (board[1][0] === "O") && (board[2][0] === "O")) ||
+        ((board[0][1] === "O") && (board[1][1] === "O") && (board[2][1] === "O")) || // columns matching
+        ((board[0][2] === "O") && (board[1][2] === "O") && (board[2][3] === "O")) ||
 
-        ((board[0][0] == "O") && (board[1][1] == "O") && (board[2][2] == "O")) || // diagonals
-        ((board[0][2] == "O") && (board[1][1] == "O") && (board[2][0] == "O"))) {
+        ((board[0][0] === "O") && (board[1][1] === "O") && (board[2][2] === "O")) || // diagonals
+        ((board[0][2] === "O") && (board[1][1] === "O") && (board[2][0] === "O"))) {
         result = "O wins";
     } else { result = "Draw!" }
 
